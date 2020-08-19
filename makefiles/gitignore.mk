94e43772934e94bfd67f12f84bf1c09487e91cfa
sunakan <sunakan03@gmail.com>
2020-08-19 22:13:52 +0000

    gitignoreにterraformを追加

--- makefiles/gitignore.mk
+++ makefiles/gitignore.mk
@@ -4,7 +4,7 @@
 IGNORE_OS       := linux,macos,windows
 IGNORE_EDITOR   := vim,emacs,intellij+all,visualstudiocode
 IGNORE_LANGUAGE := c,c++,rust,python,ruby,rust,go,java,kotlin,node,erlang,elixir,commonlisp,racket
-IGNORE_TOOL     := vagrant,ansible
+IGNORE_TOOL     := vagrant,ansible,terraform
 IGNORE_LIST     := $(IGNORE_LANGUAGE),$(IGNORE_OS),$(IGNORE_EDITOR),$(IGNORE_TOOL)
 GIT_IGNORE_URL  := https://www.toptal.com/developers/gitignore/api/$(IGNORE_LIST)
 
